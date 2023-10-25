eaf428bc46f8c2b186e7af3a17733dcbac40ca4d
German <germanmartinezgross@outlook.com>
2023-10-25 16:28:26 -0300

    first commit

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["andina/abastecimiento/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"andina/abastecimiento/test/unit/controller/Main.controller"
+], function () {
+	"use strict";
+});
